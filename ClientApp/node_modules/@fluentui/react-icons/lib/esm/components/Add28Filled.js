ed50dc8c2a458312314c75521cc0e2da52496a49
Micah Peterson <micahpeterson@live.com>
2021-11-09 14:38:40 -0600

    removed node modules and added to gitignore

--- ClientApp/node_modules/@fluentui/react-icons/lib/esm/components/Add28Filled.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import wrapIcon from '../utils/wrapIcon';
-const rawSvg = (iconProps) => {
-    const { className, primaryFill } = iconProps;
-    return React.createElement("svg", { width: 28, height: 28, viewBox: "0 0 28 28", xmlns: "http://www.w3.org/2000/svg", className: className },
-        React.createElement("path", { d: "M24 13h-9V4a1 1 0 10-2 0v9H4a1 1 0 100 2h9v9a1 1 0 102 0v-9h9a1 1 0 100-2z", fill: primaryFill }));
-};
-const Add28Filled = wrapIcon(rawSvg({}), 'Add28Filled');
-export default Add28Filled;
